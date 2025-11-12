d169dbf93f30c36e131a957d14a01b004c906922
lalakarachi302-collab <lalakarachi302@gmail.com>
2025-11-12 23:15:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "IK~3YNlXRgT#sm8GxWsbWXFkW-l8O3zO1qUtzQlEigeQz-Aej0SPCLc",  // Your bot's session ID (keep it secure)
     XDEV: process.env.XDEV || "",  // Github Username 
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "off", // on/off chat bot 
